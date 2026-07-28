b33550c1ca206282b3bc3d59f50b24a6cbbb2c70
joaovml20 <joaolima15257@gmail.com>
2026-07-27 22:28:41 -0300

    pasta e arquivo para console.log

---
+++ consolelog/consolelog.js
@@ -0,0 +1,6 @@
+console.log('ola mundo')
+//o comando acima, console.log faz com que gere uma saida de dados no terminal
+//no caso, como ele esta recebendo 'ola mundo' significar que ele ira imprime
+//'ola mundo' no terminal
+//a passagem direta de texto tem que esta contida por ' ' mas nao se preocupe
+//apenas o texto que esta sendo contido ira ser impresso no terminal
