58571111b71a27cd5f2e0abbca04fcec66b4dafa
joaovml20 <joaolima15257@gmail.com>
2026-07-27 22:21:13 -0300

    pastas e arquivos para estrutura basica e comentarios

---
+++ comentarios/comentarios.js
@@ -0,0 +1,4 @@
+//isso é um comentario em javascript
+//comentarios sao uteis para explicar parte dos codigos sem afeta-los
+//pos os comentario sao ignorados pelos compiladores e interpretadores
+//sendo assim, sua unica funcao e de comentar sobre o codigo
---
+++ estruturabasica/estruturabasica.js
@@ -0,0 +1,4 @@
+//para escrever codigo em javascript
+//use um arquivo com a extensao .js
+//um exemplo de arquivo javascript e esse proprio arquivo:
+//estruturabasica.js
