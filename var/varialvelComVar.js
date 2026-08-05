46ab6db3ec4bc212c332591967b0c6d31f3670e0
joaovml20 <joaolima15257@gmail.com>
2026-08-05 14:31:50 -0300

    pasta e arquivo para var, declação de variavel

---
+++ var/varialvelComVar.js
@@ -0,0 +1,11 @@
+// A palavra var serve para declarar uma variavel no javascript
+// uma variavel nada mais é que um local na memoria para armazenar informações
+//No javascript, as variaveis devem começar com numero ou letra, após isso é permitido usar caracteres
+//especias como underline "_" ou cifrão "$"
+// um exemplo abaixo de uma declaração de uma variavel
+var souUmaVariavel = "ola mundo"
+// acima esta a declaração da variavel souUmaVariavel com var, que ela por sua vez, armazenar um texto
+// que é "ola mundo"
+// javascript diferencia maiusculo de minusculo e vise-versa, ou seja, se no programar tivesse outra variavel com o nome SouUmaVariavel seria diferente da variavel souUmaVariavel por conta do "S" maiusculo.
+// javascript usar o padrão de nomeação de variavel de camelCase, onde a primeira palavra da variavel é minuscula e após ela, a primeira letra das palavras subsequentes são maiusculas
+// exemplo: souUMaVariavel = sou - Uma - Variavel 
