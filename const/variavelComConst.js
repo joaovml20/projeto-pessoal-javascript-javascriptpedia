621b37c120877fd0e82c60a049645b638cb57a4b
joaovml20 <joaolima15257@gmail.com>
2026-08-06 12:52:09 -0300

    declaração de variaveis com const, pasta e arquivos iniciais

---
+++ const/variavelComConst.js
@@ -0,0 +1,12 @@
+// Com a palavra const é possivel declarar variaveis constantes
+//uma constante nada mais é que uma variavel que não pode ter seu valor modificado
+// uma variavel nada mais é que um local na memoria para armazenar informações
+//No javascript, as variaveis devem começar com numero ou letra, após isso é permitido usar caracteres
+//especias como underline "_" ou cifrão "$"
+// um exemplo abaixo de uma declaração de uma variavel
+const souUmaVariavel = "ola mundo"
+// acima esta a declaração da variavel souUmaVariavel com const, que ela por sua vez, armazenar um texto
+// que é "ola mundo" e ela não pode ter seu valor alterado para outro texto ou numero
+// javascript diferencia maiusculo de minusculo e vise-versa, ou seja, se no programar tivesse outra variavel com o nome SouUmaVariavel seria diferente da variavel souUmaVariavel por conta do "S" maiusculo.
+// javascript usar o padrão de nomeação de variavel de camelCase, onde a primeira palavra da variavel é minuscula e após ela, a primeira letra das palavras subsequentes são maiusculas
+// exemplo: souUMaVariavel = sou - Uma - Variavel
