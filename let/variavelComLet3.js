73413ec1ed0acfb4ad9c1c94a2806701e07130e1
joaovml20 <joaolima15257@gmail.com>
2026-08-07 15:19:08 -0300

    tipos de variaveis

---
+++ let/variavelComLet3.js
@@ -0,0 +1,27 @@
+//No javascript, existem alguns tipos de dados que podem ser armazenados nas variaveis ou se preferir
+//tipos de variaveis, vejamos 5 tipos de dados destas variaveis
+// os Boolean, os Number, As String, os null, os undefined
+//abaixo, alguns exemplos destes dados nas variaveis
+
+let souUmBoolean = true
+let nãoSouUmBoolean = false
+// acima duas variaveis que contem dados boleanos ou simplesmente, variaveis do tipo booleano
+// essas variaveis armazenam os valores verdadeiro (true) e falso (false)
+// esses valores são uteis na logica do programar em certas ocasiões
+
+let souUmNumero = 10
+let souOutroNumero = 3.14
+//acima variaveis do tipo number, elas contem dados numericos que podem ser tanto sem ponto flutuante
+// como com ponto flutuante, igual 3.14, o ponto flutuante aqui e um ponto final, não uma virgula
+// são usados para calculos no programar
+
+let souUmaSring = "buooo"
+//acima uma variavel do tipo string, ela apenas armazenar um texto que pode ser impresso ou utilizado
+// de alguma maneira no decorrer do programar
+
+let souUmVazio = null
+//acima uma variavel do tipo null, ela como proprio nome diz, armazenar um vazio ou melhor é ausente de valor
+
+let souUmaVariavel
+//acima uma variavel do tipo undefined, esse tipo de variavel significar que a variavel foi declarada 
+// no programar mas não foi atribuido nem um valor a ela
