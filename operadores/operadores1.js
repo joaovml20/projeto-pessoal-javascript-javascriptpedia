716efc9725b79184e958a5dfeb0647b6745ae40f
joaovml20 <joaolima15257@gmail.com>
2026-08-15 15:27:33 -0300

    pasta e arquivo para operadores, operadores de atribuicao

---
+++ operadores/operadores1.js
@@ -0,0 +1,29 @@
+// existem varios operadores no javascript, os operadores nada mais sao que simbolos que executam algo, desde de uma operacao matematica ou guardar um valor em uma variavel
+//vejamos alguns abaixo
+
+let numero1 = 10
+// o operador de atribuição(=) atribui um valor a uma variavel a esquerda com base no valor da direita
+// nesse caso a variavel a esquerda(numero1) recebe o valor a direita(10)
+
+let numero2 = 1
+numero2 += 10
+// o operador += somar o valor da variavel a esquerda pelo valor a direita e atribui a variavel
+// no caso acima, a variavel numero2 tinha 1, entao 1 é somado a 10 que retorna 11 e 11 é atribuido como valor a variavel numero2
+
+let numero3 = 10
+numero3 -= 1
+// o operador -= é similar a += mas ele ao inves de somar, ele subtrai
+// no caso acima, a variavel numero3 tinha 10, entao graças a -=, ele faz a operacao "10 - 1" que o resultado dessa operacao tem como resultado 9 e entao 9 é o novo valor da variavel numero3
+
+let numero4 = 10
+numero4 *= 10
+// o operador *= executar a multiplicacao do valor da variavel a esquerda com o valor a direita e atribui o resultado da operacao a variavel
+// na situacao acima, a variavel numero4 tem o valor 10 que entao e multiplicado por 10 e entao o resultado e atribuido a variavel numero4
+
+let numero5 = 10
+numero5 /= 2
+// o operador /= é similar ao operador *= mas ao inves de executar uma operacao de multiplicacao, ele executar uma divisao
+
+let numero6 = 10
+numero6 %= 3
+// o operador %= tambem executar uma divisao assim como /= porem o que ele atribui a variavel e o resto da divisao
