bd237b8c16d04019989faa4a7a6ded613c224b8d
joaovml20 <joaolima15257@gmail.com>
2026-08-18 14:59:47 -0300

    operadores, operadores aritméticos

---
+++ operadores/operadores3.js
@@ -0,0 +1,32 @@
+// mais alguns operadore em javascript são os operadores aritméticos
+// eles nada mais fazem que executar operações mátematicas, vejamos alguns abaixo:
+
+let numero1 = 1
+console.log(numero1 + 1)
+// o operador de adição(+) executar uma somar entre o valor da esquerda e da direita
+// no caso acima, numero1 que armazenar 1 é somado com mais 1, logo 2 é o que sera impresso no console.log
+
+let numero2 = 1
+console.log(numero2 - 1)
+// o operador de subtração(-) executar uma subtração entre o valor da esquerda e da direita
+// no caso acima, numero2 que armazenar 1 é subtraido com 1, logo 0 é o que sera impresso no console.log
+
+let numero3 = 2
+console.log(numero3 * 3)
+// o operador de multiplicação(*) executar uma multiplicação entre o valor da esquerda e da direita
+// no caso acima, numero3 que armazenar 2 é multiplicado com 3, logo 6 é o que sera impresso no console.log
+
+let numero4 = 2
+console.log(numero4 / 2)
+// o operador de divisão(/) executar uma divisão entre o valor da esquerda e da direita
+// no caso acima, numero4 que armazenar 2 é dividido por 2, logo 1 é o que sera impresso no console.log
+
+let numero5 = 3
+console.log(numero5 % 2)
+// o operador de Módulo ou resto da divisão(%) executar uma divisão entre o valor da esquerda e da direita porem diferente do  operador de divisão, ele retornara o resto da divisão
+// no caso acima, numero5 que armazenar 3 é dividido por 2, o resultado dessa operação é 2 no entanto restar 1 e esse 1 é o que será impresso no console.log
+
+let numero6 = 5
+console.log(numero6 ** 2)
+// o operador de Exponenciação(**) executar uma multiplicação entre o valor da esquerda vezes ele mesmo e a quantidade de vezes que determinara isso é o valor a direita
+// no caso acima, numero6 armazenar o valor 5 que é multiplicado por ele mesmo 2 vezes, ou seja 5 * 5, onde o resultado dessa operação sera impresso no console.log
