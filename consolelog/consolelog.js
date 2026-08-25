82692d241444eb0a8d6021f39a5e1c717c95b911
joaovml20 <joaolima15257@gmail.com>
2026-08-25 14:50:05 -0300

    atualizacao no console.log, adicionando informacoes, imprime variaveis

--- consolelog/consolelog.js
+++ consolelog/consolelog.js
@@ -1,6 +1,10 @@
 console.log('ola mundo')
 //o comando acima, console.log faz com que gere uma saida de dados no terminal
 //no caso, como ele esta recebendo 'ola mundo' significar que ele ira imprime
-//'ola mundo' no terminal
-//a passagem direta de texto tem que esta contida por ' ' mas nao se preocupe
-//apenas o texto que esta sendo contido ira ser impresso no terminal
+//ola mundo no terminal
+//a passagem direta de texto tem que esta contida por ' '
+
+// tambem e possivel imprimir valores de variaveis no console.log
+let mensagem = 'ola mundo'
+console.log(mensagem)
+// no caso acima, sera impresso o valor da variavel mensagem que ela por sua vez, contem 'ola mundo'
