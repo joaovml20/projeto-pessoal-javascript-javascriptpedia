0b69712eb9cfd7b1bf808698e4dfd66f0083a7e4
joaovml20 <joaolima15257@gmail.com>
2026-08-27 15:00:59 -0300

    estrutura de controle, estrutura for, rascunho

---
+++ estruturasDeControle/for.js
@@ -0,0 +1,9 @@
+// No javascript existem as estruturas de controle de repetição, uma delas é o for
+// a estrutura for tem 4 partes, a inicialização, a condição, o incremento ou decremento e por fim o bloco de codigos que sera repetido
+// a inicialização é a variavel contadora do for, essa variavel pode ser tanto declarada dentro do for como fora dele
+// a condição é a expressão boleana que enquanto retorna verdadeiro, o bloco de comandos do for continuara repetindo
+// o incremento ou decremento é a adição ou subtração da variavel contadora a cada repetição do bloco de comandos do for
+// um exemplo abaixo
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
